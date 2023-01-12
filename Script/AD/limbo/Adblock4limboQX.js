eac64ca084959f6ba3aa9e2c427b15d56bbf998c
OocMe <103792636+OocMe@users.noreply.github.com>
2023-01-12 22:30:55 +0800

    Update Adblock4limboQX.js

--- Script/AD/limbo/Adblock4limboQX.js
+++ Script/AD/limbo/Adblock4limboQX.js
@@ -1,7 +1,7 @@
 // 引用地址 https://github.com/limbopro/Adblock4limbo/blob/main/Adguard/Adblock4limbo.js
 
 let ele = '<head>';
-let eleReplace = '<head><link rel="stylesheet" href="https://raw.githubusercontent.com/OocMe/ONE/main/Script/AD/limbo/Adblock4limbo.user.css" type="text/css" /><script type="text/javascript" async="async" src="https://raw.githubusercontent.com/OocMe/ONE/main/Script/AD/limbo/Adblock4limbo.user.js"></script>'
+let eleReplace = '<head><link rel="stylesheet" href="https://limbopro.com/CSS/Adblock4limbo.user.css" type="text/css" /><script type="text/javascript" async="async" src="https://limbopro.com/Adguard/Adblock4limbo.user.js"></script>'
 let body = $response.body
     .replace(ele, eleReplace)
 $done({body});

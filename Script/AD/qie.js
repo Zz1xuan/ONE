01901b6ef4aff4c60c90a71b4149fb53377605b1
OocMe <103792636+OocMe@users.noreply.github.com>
2023-02-13 11:10:10 +0800

    Update qie.js

--- Script/AD/qie.js
+++ Script/AD/qie.js
@@ -1,3 +1,10 @@
+
+^https?:\/\/userapi\.qiekj\.com\/slot\/get url reject-200
+^https?:\/\/userapi\.qiekj\.com\/appTitle\/get url script-response-body https://raw.githubusercontent.com/OocMe/ONE/main/Script/AD/qie.js
+^https?:\/\/qemyapi\.qiekj\.com\/api\/item_list url script-response-body https://raw.githubusercontent.com/OocMe/ONE/main/Script/AD/qie.js
+
+hostname = userapi.qiekj.com, qemyapi.qiekj.com,
+
 let obj = JSON.parse($response.body);
 
 obj = {

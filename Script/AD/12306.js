02da28b2ddffe95db4387ae7d8872ede887233bb
OocMe <103792636+OocMe@users.noreply.github.com>
2023-02-22 10:18:16 +0800

    Update 12306.js

--- Script/AD/12306.js
+++ Script/AD/12306.js
@@ -5,7 +5,7 @@
 请检查你的分流是否有ad.12306.cn，如果有，需要去除（可使用解析器排出#out=12306），不然重写是无法生效的，同时检查是否有与其冲突的重写
 
 [rewrite_local]
-^https?:\/\/ad\.12306\.cn\/ad\/ser\/getAdList url script-response-body https://github.com/ddgksf2013/Scripts/raw/master/12306.js
+^https?:\/\/ad\.12306\.cn\/ad\/ser\/getAdList url script-analyze-echo-response https://github.com/ddgksf2013/Scripts/raw/master/12306.js
 
 [mitm]
 hostname = ad.12306.cn
@@ -22,5 +22,6 @@ hostname = ad.12306.cn
 
 
 
+const version = 'V1.0.20';
 
-!function(){let e;e="0007"===JSON.parse($request.body).placementNo?'{"materialsList":[{"billMaterialsId":"6491","filePath":"http://bing.ioliu.cn","creativeType":1}],"advertParam":{"skipTime":1}}':'{"code":"00","message":"无广告返回"}';$done({body:e})}();
+var obj=JSON.parse($request.body),ddgksf2013={};"0007"==obj.placementNo?(ddgksf2013.materialsList=[{billMaterialsId:"6491",filePath:"https://bing.img.run/1366x768.php",creativeType:1}],ddgksf2013.advertParam={skipTime:1},ddgksf2013.code="00"):ddgksf2013={code:"00",message:"无广告返回"},$done({body:JSON.stringify(ddgksf2013)});

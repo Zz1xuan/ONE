f895fba4bb139669395664c1372b366a02e0a846
OocMe <103792636+OocMe@users.noreply.github.com>
2023-02-22 17:17:53 +0800

    Update 12306.js

--- Script/AD/12306.js
+++ Script/AD/12306.js
@@ -19,16 +19,4 @@ hostname = ad.12306.cn
 
 
 
-const version = 'V1.0.20';
-
-var obj=JSON.parse($request.body),ddgksf2013={};
-
-if ("0007" == obj.placementNo) {
-  ddgksf2013.materialsList=[{billMaterialsId:"6491",filePath:"https://www.bing.com/HPImageArchive.aspx?format=js&idx=0&n=1",creativeType:1}],
-  ddgksf2013.advertParam={skipTime:1},
-  ddgksf2013.code="00"
-} else {
-  ddgksf2013={code:"00",message:"无广告返回"}
-}
-
-$done({body:JSON.stringify(ddgksf2013)});
+!function(){let e,i=JSON.parse($request.body);e="0007"===i.placementNo?'{"materialsList":[{"billMaterialsId":"255","filePath":"h","creativeType":1}],"advertParam":{"skipTime":1}}':"G0054"===i.placementNo?'{"code":"00","materialsList":[{}],"advertParam":{"skipTime":3000,"showSkipBtn":0,"skipTimeAgain":0,"chacheTime":600000,"fixedscreen":3,"isDefault":0,"displayNumDi":1,"index":2}}':'{"code":"00","message":"无广告返回"}';"undefined"!=typeof $task?$done({body:e}):$done({response:{body:e}})}();

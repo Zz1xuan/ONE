f26974e7a5f8a2f2f8c3a27b14bb2cf5fd1c5bf5
OocMe <103792636+OocMe@users.noreply.github.com>
2023-02-22 13:25:09 +0800

    Update yingshi.js

--- Script/AD/yingshi.js
+++ Script/AD/yingshi.js
@@ -1,8 +1,10 @@
-var jsonData = JSON.parse($response.body);
-var tabItems = jsonData.grayConfigInfos["155"].switchStatus;
-tabItems = JSON.parse(tabItems);
-tabItems.tabitems = tabItems.tabitems.filter(function(item) {
-  return item.title !== "萤石" && item.title !== "商城" && item.title !== "发现";
-});
-jsonData.grayConfigInfos["155"].switchStatus = JSON.stringify(tabItems);
-$done({body: JSON.stringify(jsonData)});
+// 将JSON字符串解析为JavaScript对象
+const jsonObj = JSON.parse(jsonStr);
+
+// 找到要删除的元素，并从数组中删除它们
+jsonObj.grayConfigInfos["155"].switchStatus = JSON.parse(jsonObj.grayConfigInfos["155"].switchStatus);
+jsonObj.grayConfigInfos["155"].switchStatus.tabitems = jsonObj.grayConfigInfos["155"].switchStatus.tabitems.filter((item) => item.title !== "智能" && item.title !== "商城" && item.title !== "发现");
+jsonObj.grayConfigInfos["155"].switchStatus = JSON.stringify(jsonObj.grayConfigInfos["155"].switchStatus);
+
+// 将修改后的JavaScript对象转换回JSON字符串
+const updatedJsonStr = JSON.stringify(jsonObj);

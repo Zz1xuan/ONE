52ba6508e649efb7591a5e8c94a10004df0465ba
OocMe <103792636+OocMe@users.noreply.github.com>
2023-02-22 10:24:37 +0800

    Update 12306.js

--- Script/AD/12306.js
+++ Script/AD/12306.js
@@ -24,4 +24,4 @@ hostname = ad.12306.cn
 
 const version = 'V1.0.20';
 
-var obj=JSON.parse($request.body),ddgksf2013={};"0007"==obj.placementNo?(ddgksf2013.materialsList=[{billMaterialsId:"6491",filePath:"http://47.111.254.61:8000/bingWp",creativeType:1}],ddgksf2013.advertParam={skipTime:1},ddgksf2013.code="00"):ddgksf2013={code:"00",message:"无广告返回"},$done({body:JSON.stringify(ddgksf2013)});
+var obj=JSON.parse($request.body),ddgksf2013={};"0007"==obj.placementNo?(ddgksf2013.materialsList=[{billMaterialsId:"6491",filePath:" http://47.111.254.61:8000/bingWp ",creativeType:1}],ddgksf2013.advertParam={skipTime:1},ddgksf2013.code="00"):ddgksf2013={code:"00",message:"无广告返回"},$done({body:JSON.stringify(ddgksf2013)});

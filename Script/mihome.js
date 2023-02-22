e237624eadddb4697613a543471bd61a46b9e573
OocMe <103792636+OocMe@users.noreply.github.com>
2023-02-22 11:31:46 +0800

    Update mihome.js

--- Script/mihome.js
+++ Script/mihome.js
@@ -1,21 +1,3 @@
-const urlRegex = /^https:\/\/home\.mi\.com\/cgi-op\/api\/v1\/recommendation\/myTab\?/;
-// 圈x处理HTTP响应的函数
-function responseHandler(responseDetails) {
-  // 如果URL匹配
-  if (urlRegex.test(responseDetails.url)) {
-    // 将响应文本解析为JSON对象
-    var jsonResponse = JSON.parse(responseDetails.responseText);
-    // 删除banners字段
-    delete jsonResponse.banners;
-    // 将修改后的JSON对象转换为字符串，并更新响应文本
-    responseDetails.responseText = JSON.stringify(jsonResponse);
-  }
-  // 返回修改后的响应
-  return {
-    response: responseDetails.response,
-    status: responseDetails.status,
-    headers: responseDetails.headers
-  };
-}
-
-$done({response: responseHandler({response: $response.response})});
+$done({ 
+  body: $response.body.replace(/"banners":{[^}]*},/,'')
+});

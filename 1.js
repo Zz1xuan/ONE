fffa726d46d7f1d4c96387d81e43ab0877ad6a3e
OocMe <103792636+OocMe@users.noreply.github.com>
2023-03-01 15:41:26 +0800

    Update 1.js

--- 1.js
+++ 1.js
@@ -1,3 +1,10 @@
-var data = JSON.parse($response.body);
-delete data.right_ad;
+// 获取数据
+var data = $response.body;
+
+// 如果数据存在right_ad属性，就删除它
+if (data.hasOwnProperty('right_ad')) {
+  delete data.right_ad;
+}
+
+// 返回修改后的数据
 $done({body: JSON.stringify(data)});

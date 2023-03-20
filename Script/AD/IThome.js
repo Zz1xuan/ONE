5289a70d355b36f0e369f9487aff7a994df46e6b
OocMe <103792636+OocMe@users.noreply.github.com>
2023-03-20 09:27:54 +0800

    Update IThome.js

--- Script/AD/IThome.js
+++ Script/AD/IThome.js
@@ -1,45 +1,41 @@
-// 2023-01-06 20:20
-
-if (!$response.body) $done({});
+/*
+引用脚本https://raw.githubusercontent.com/toulanboy/scripts/master/ithome_ad/ithome_ad.js
+*/
 const url = $request.url;
-let obj = JSON.parse($response.body);
-
-// IT之家-appList
-if (url.includes("/json/listpage/news") || url.includes("/json/newslist/news")) {
-  if (obj.newslist) {
-    obj.newslist = obj.newslist.filter((n) => !n.aid);
-  }
-} else if (url.includes("/json/slide/index")) {
-  // IT之家-appSlide
-  const newList = obj.filter((i) => !i.isad);
-  obj.splice(0, obj.length);
-  obj.push(...newList);
-} else if (url.includes("/api/news/newslistpageget")) {
-  // IT之家-mobileWeb
-  if (obj.Result) {
-    obj.Result = obj.Result.filter((r) =>
-      r.NewsTips.every((t) => t.TipName !== "广告")
-    );
-  }
-} else if (url.includes("/api/news/index") || url.includes("/api/topmenu/getfeeds")) {
-  // IT之家-newAppFeed
-  let list = obj.data.list;
-  const newList = [];
-  for (const item of list) {
-    if (
-      item.feedContent.smallTags &&
-      item.feedContent.smallTags.some((s) => s.text === "广告")
-    ) {
-      continue;
+let body = JSON.parse($response.body);
+if (url.indexOf("newslist") != -1 || url.indexOf("listpage") != -1) {
+    let i = body.newslist.length;
+    while (i--) {
+        if (body.newslist[i].hasOwnProperty('aid')) {
+            body.newslist.splice(i, 1);
+        }
     }
-    if (item.feedContent.focusNewsData) {
-      const newNewsData = item.feedContent.focusNewsData.filter((n) => !n.isAd);
-      item.feedContent.focusNewsData = newNewsData;
+} else if (url.indexOf("slide") != -1) {
+    let i = body.length;
+    while (i--) {
+        if (body[i].isad || body[i].link.indexOf("jd.com") != -1 || body[i].link.indexOf("taobao.com") != -1) {
+            body.splice(i, 1);
+        }
     }
-    newList.push(item);
-  }
-  obj.data.list = newList;
+} else if (url.indexOf("napi") != -1) {
+    let listData = body.data.list;
+    let i = listData.length;
+    while (i--) {
+        if (listData[i].feedType == 10002) {
+            let j = listData[i].feedContent.focusNewsData.length;
+            while (j--) {
+                if (listData[i].feedContent.focusNewsData[j].isAd) {
+                    listData[i].feedContent.focusNewsData.splice(j, 1);
+                }
+            }
+        } else if (listData[i].feedType == 10000) {
+            if (listData[i].feedContent.smallTags[0].text != null && listData[i].feedContent.smallTags[0].text.indexOf("广告") != -1) {
+                listData.splice(i, 1)
+            }
+        }
+    }
 }
-
-body = JSON.stringify(obj);
-$done({ body });
+body = JSON.stringify(body)
+$done({
+    body
+})

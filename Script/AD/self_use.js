9a04369942a6fa1a3bc8bc2b4917c5b0e1fc53fc
OocMe <103792636+OocMe@users.noreply.github.com>
2023-03-31 13:18:25 +0800

    Update self_use.js

--- Script/AD/self_use.js
+++ Script/AD/self_use.js
@@ -1,11 +1,17 @@
-
 const url = $request.url;
 const header = $request.headers;
-let ua = header["User-Agent"];
 
-if (url.includes("/amdc/mobileDispatch")) {
+if ("undefined" !== typeof $task) {
+  let ua = header["User-Agent"];
   if (ua.includes("AMap") || ua.includes("Cainiao") || ua.includes("%E9%A3%9E%E7%8C%AA%E6%97%85%E8%A1%8C")) {
-    $done({ status: "HTTP/1.1 404 Not Found", body: "" });
+    $done({ status: "HTTP/1.1 404 Not Found" });
+  } else {
+    $done({});
+  }
+} else {
+  let ua = header["user-agent"];
+  if (ua.includes("AMap") || ua.includes("Cainiao")) {
+    $done();
   } else {
     $done({});
   }

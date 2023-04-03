446d52ac2a9b2f4229f2fa013841ecca4a885d5d
OocMe <103792636+OocMe@users.noreply.github.com>
2023-04-03 18:50:09 +0800

    Update ali.js

--- Script/AD/ali.js
+++ Script/AD/ali.js
@@ -1,18 +1,6 @@
 let body = $response.body;
 if (body) {
   switch (true) {
-    case /adrive\/v1\/user\/driveCapacityDetails/.test($request.url):
-      let keyr = JSON.parse($response.body);
-      keyr.drive_total_size = 55834574848000; //详情
-      body = JSON.stringify(keyr);
-      break;
-    case /v2\/databox\/get_personal_info/.test($request.url):
-      let keyss = JSON.parse($response.body); keyss.personal_space_info.total_size = 57174604644352;
-      body = JSON.stringify(keyss); //我的,单位字节(B)
-      break;
-    case /business\/v1\.0\/users\/feature\/list/.test($request.url):
-      body = body.replace(/intercept":true/g, 'intercept": false').replace(/name":"(.*?)"/g, 'name": "vip"');
-      break;
     case /apps\/v1\/users\/home\/widgets/.test($request.url):
       let home = JSON.parse(body);
       const coreFeatures = home.coreFeatures;
@@ -38,14 +26,6 @@ if (body) {
       ];
       removeKeys.forEach(key => delete home[key]);
       body = JSON.stringify(home);
-      break;
-    case /apps\/v1\/users\/apps\/welcome/.test($request.url):
-      let tip = {
-        "title": "key",
-        "description": "摆烂云盘",
-        "emojis": ["🍉"]
-      };
-      body = JSON.stringify(tip);
       break;
     default:
       $done({})

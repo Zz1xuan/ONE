ba168d4409a582c6baa60aa6f489bc1cbfebff07
OocMe <103792636+OocMe@users.noreply.github.com>
2023-04-13 18:48:45 +0800

    Update 1.js

--- 1.js
+++ 1.js
@@ -1,10 +1,5 @@
-
-// 假设服务端返回的 JSON 数据保存在变量 data 中
-// 解析 JSON 数据
-var jsonData = JSON.parse(data);
-// 删除 data 属性
+var response = $response.body;
+var jsonData = JSON.parse(response);
 delete jsonData.data;
-// 将修改后的 JSON 数据转换回字符串
 var modifiedData = JSON.stringify(jsonData);
-// 返回修改后的 JSON 数据
-return modifiedData;
+$done(modifiedData);

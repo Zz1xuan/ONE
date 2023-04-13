e96595892366048b74110e88aba54371923a9f57
OocMe <103792636+OocMe@users.noreply.github.com>
2023-04-13 14:38:45 +0800

    Update 1.js

--- 1.js
+++ 1.js
@@ -1,15 +1,6 @@
-if ($response.status === 200) {
-  const body = JSON.parse($response.body);
-  if (body && typeof body === "object") {
-    if (body.home) {
-      delete body.home;
-      $done({ body: JSON.stringify(body) });
-    } else {
-      $done({});
-    }
-  } else {
-    $done({});
-  }
-} else {
-  $done({});
-}
+let body = JSON.parse($response.body);
+
+delete body.admin;
+delete body.home;
+
+$done({ body: JSON.stringify(body) });

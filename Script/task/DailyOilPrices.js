86435ecd161ac6cea369fbce149200f8a0de5e56
OocMe <103792636+OocMe@users.noreply.github.com>
2023-04-27 19:30:13 +0800

    Update DailyOilPrices.js

--- Script/task/DailyOilPrices.js
+++ Script/task/DailyOilPrices.js
@@ -1,9 +1,15 @@
-//  作者deezertidal
-/*
-[task_local]
-0 0 8 ? * * https://raw.githubusercontent.com/OocMe/ONE/main/Script/task/DailyOilPrices.js, tag=每日油价, enabled=true
-*/
 const apiurl = "https://apis.tianapi.com/oilprice/index?key=7de401b696e9905ca60899d9945a1996&prov=%E6%B5%99%E6%B1%9F";
+
+const $tool = {
+    notify: (title, subtitle, message) => {
+        if ($task) {
+            $notify(title, subtitle, message);
+        }
+        if ($notification) {
+            $notification.post(title, subtitle, message);
+        }
+    }
+};
 
 $task.fetch({ url: apiurl }).then(response => {
     var obj = JSON.parse(response.body);
@@ -13,7 +19,7 @@ $task.fetch({ url: apiurl }).then(response => {
     var p95 = "95号汽油:" + "¥" + obj.result.p95 + "\xa0\xa0\xa0";
     var p98 = "98号汽油:" + "¥" + obj.result.p98 + "\xa0\xa0\xa0";
     var time = obj.result.time;
-    $notify(prov + "油价提醒", time, p92 + p95 + p98 + p0);
+    $tool.notify(prov + "油价提醒", time, p92 + p95 + p98 + p0);
     $done();
 }, reason => {
     console.log(reason.error);

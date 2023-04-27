dafb7f32c65528728e114d0cf05404c9e5742530
OocMe <103792636+OocMe@users.noreply.github.com>
2023-04-27 19:47:38 +0800

    Update DailyOilPrices.js

--- Script/task/DailyOilPrices.js
+++ Script/task/DailyOilPrices.js
@@ -1,14 +1,14 @@
-const apiurl = "?key=7de401b696e9905ca60899d9945a1996&prov=%E6%B5%99%E6%B1%9F";
+const apiurl = "https://apis.tianapi.com/oilprice/index??key=7de401b696e9905ca60899d9945a1996&prov=%E6%B5%99%E6%B1%9F";
 
-$task.fetch({ url: apiurl }).then(response => {
-    var obj = JSON.parse(response.body);
-    var prov = obj.result.prov;
-    var p0 = "0号柴油:" + "¥" + obj.result.p0 + "\xa0\xa0\xa0";
-    var p92 = "92号汽油:" + "¥" + obj.result.p92 + "\xa0\xa0\xa0";
-    var p95 = "95号汽油:" + "¥" + obj.result.p95 + "\xa0\xa0\xa0";
-    var p98 = "98号汽油:" + "¥" + obj.result.p98 + "\xa0\xa0\xa0";
-    var time = obj.result.time;
-    $notify(prov + "油价提醒", time, p92 + p95 + p98 + p0);
+$http.get(apiurl).then(response => {
+    const obj = response.data;
+    const prov = obj.result.prov;
+    const p0 = `0号柴油: ¥${obj.result.p0}\xa0\xa0\xa0`;
+    const p92 = `92号汽油: ¥${obj.result.p92}\xa0\xa0\xa0`;
+    const p95 = `95号汽油: ¥${obj.result.p95}\xa0\xa0\xa0`;
+    const p98 = `98号汽油: ¥${obj.result.p98}\xa0\xa0\xa0`;
+    const time = obj.result.time;
+    $notify(`${prov}油价提醒`, time, `${p92}${p95}${p98}${p0}`);
     $done();
 }, reason => {
     console.log(reason.error);

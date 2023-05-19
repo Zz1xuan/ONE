e8298940d63bbc3455e2f4c3057a4cf338aa1fab
OocMe <103792636+OocMe@users.noreply.github.com>
2023-05-19 19:27:39 +0800

    Update jingxi.js

--- Script/AD/jingxi.js
+++ Script/AD/jingxi.js
@@ -1,14 +1,7 @@
-let obj = JSON.parse($response.body);
-
-/* 京喜APP delivery_show */
+let obj=JSON.parse($response.body);
+/*京喜APP*/
 if (/^https?:\/\/api\.m\.jd\.com\/api\?functionId=delivery_show/.test($request.url)) { 
-    obj.data.materialList.startTime = 3667476800000;
-    obj.data.materialList.endTime = 3667908800000;
+    obj.data.materialList.startTime=3667476800000;
+    obj.data.materialList.endTime  =3667908800000;
 }
-
-/* 京喜特价APP lite_advertising */
-if (/^https?:\/\/api\.m\.jd\.com\/client\.action\?functionId=lite_advertising/.test($request.url)) {
-    delete obj.data.jdLiteAdvertisingVO;
-}
-
-$done({ body: JSON.stringify(obj) });
+$done({body:JSON.stringify(obj)});

5af5e343b2eb82e7e3b8aa1ba8f61aa8f3579ff2
OocMe <103792636+OocMe@users.noreply.github.com>
2023-05-25 22:25:05 +0800

    Update qie.js

--- Script/AD/qie.js
+++ Script/AD/qie.js
@@ -1,9 +1,7 @@
 [rewrite_local]
 
 ^https?:\/\/userapi\.qiekj\.com\/slot\/get url reject
-^https?:\/\/userapi\.qiekj\.com\/(appTitle\/get|api\/item_list|item_category\/list) url reject
-
-#^https?:\/\/userapi\.qiekj\.com\/appTitle\/get url script-response-body https://raw.githubusercontent.com/OocMe/ONE/main/Script/AD/qie.js
+^https?:\/\/userapi\.qiekj\.com\/(appTitle\/get|api\/item_list|item_category\/list) url script-response-body https://raw.githubusercontent.com/OocMe/ONE/main/Script/AD/qie.js
 #^https?:\/\/qemyapi\.qiekj\.com\/api\/item_list url script-response-body https://raw.githubusercontent.com/OocMe/ONE/main/Script/AD/qie.js
 #^https?:\/\/qemyapi\.qiekj\.com\/item_category\/list url script-response-body https://raw.githubusercontent.com/OocMe/ONE/main/Script/AD/qie.js
 

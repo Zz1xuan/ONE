544818ae7cc9e8792d0c2c2d78d9192649c1e086
OocMe <103792636+OocMe@users.noreply.github.com>
2023-05-25 22:15:18 +0800

    Update qie.js

--- Script/AD/qie.js
+++ Script/AD/qie.js
@@ -3,20 +3,12 @@
 ^https?:\/\/userapi\.qiekj\.com\/slot\/get url reject
 ^https?:\/\/userapi\.qiekj\.com\/appTitle\/get url script-response-body https://raw.githubusercontent.com/OocMe/ONE/main/Script/AD/qie.js
 ^https?:\/\/qemyapi\.qiekj\.com\/api\/item_list url script-response-body https://raw.githubusercontent.com/OocMe/ONE/main/Script/AD/qie.js
+^https?:\/\/qemyapi\.qiekj\.com\/item_category\/list url script-response-body https://raw.githubusercontent.com/OocMe/ONE/main/Script/AD/qie.js
 
 [mitm]
 hostname = userapi.qiekj.com, qemyapi.qiekj.com,
 
 
 let obj = JSON.parse($response.body);
-
-obj = {
-  data: {
-    total: 0,
-    datas: [{}]
-  },
-  code: 0,
-  datas: [{}]
-};
-
+delete obj.data;
 $done({ body: JSON.stringify(obj) });

a1850965d7e25abfb4f3c43123d1895fcdef06fa
OocMe <103792636+OocMe@users.noreply.github.com>
2023-05-25 22:20:54 +0800

    Update qie.js

--- Script/AD/qie.js
+++ Script/AD/qie.js
@@ -1,9 +1,11 @@
 [rewrite_local]
 
 ^https?:\/\/userapi\.qiekj\.com\/slot\/get url reject
-^https?:\/\/userapi\.qiekj\.com\/appTitle\/get url script-response-body https://raw.githubusercontent.com/OocMe/ONE/main/Script/AD/qie.js
-^https?:\/\/qemyapi\.qiekj\.com\/api\/item_list url script-response-body https://raw.githubusercontent.com/OocMe/ONE/main/Script/AD/qie.js
-^https?:\/\/qemyapi\.qiekj\.com\/item_category\/list url script-response-body https://raw.githubusercontent.com/OocMe/ONE/main/Script/AD/qie.js
+^https?:\/\/userapi\.qiekj\.com\/(appTitle\/get|api\/item_list|item_category\/list) url reject
+
+#^https?:\/\/userapi\.qiekj\.com\/appTitle\/get url script-response-body https://raw.githubusercontent.com/OocMe/ONE/main/Script/AD/qie.js
+#^https?:\/\/qemyapi\.qiekj\.com\/api\/item_list url script-response-body https://raw.githubusercontent.com/OocMe/ONE/main/Script/AD/qie.js
+#^https?:\/\/qemyapi\.qiekj\.com\/item_category\/list url script-response-body https://raw.githubusercontent.com/OocMe/ONE/main/Script/AD/qie.js
 
 [mitm]
 hostname = userapi.qiekj.com, qemyapi.qiekj.com,

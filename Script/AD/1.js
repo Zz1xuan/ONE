ce7dc9eca894c93f15e2fbfa6aab98dde5125fc6
OocMe <103792636+OocMe@users.noreply.github.com>
2023-05-25 21:38:23 +0800

    Update 1.js

--- Script/AD/1.js
+++ Script/AD/1.js
@@ -3,7 +3,7 @@ let jsonData = $response.body; // 获取的响应体
 try {
   const obj = JSON.parse(jsonData); // 解析为 JSON 对象
   if (obj && obj.data && typeof obj.data.eventStatus !== "undefined") {
-    obj.data.eventStatus = 1; // 修改 eventStatus 字段的值为 1
+    obj.data.eventStatus = 0; // 修改 eventStatus 字段的值为 1
     $done({ body: JSON.stringify(obj) }); // 返回修改后的 JSON 对象作为响应
   } else {
     console.log("无效的 JSON 格式或缺少必要的字段");

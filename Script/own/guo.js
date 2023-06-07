067bb1ea61bfec8a5059f848d2a3e5684c201053
OocMe <103792636+OocMe@users.noreply.github.com>
2023-06-07 18:53:24 +0800

    Create guo.js

---
+++ Script/own/guo.js
@@ -0,0 +1,36 @@
+let obj = JSON.parse($response.body);
+   
+    obj = {
+    "result" : {
+      "message" : "success",
+      "data" : {
+        "last_modify" : "",
+        "user_is_pro" : true,
+        "rc_id" : "",
+        "blocked" : false,
+        "permissions" : [
+          -1
+        ],
+        "group_is_pro" : true,
+        "event" : {
+          "valid" : true,
+          "id" : "",
+          "icon" : "https://img.vanemu.com/assets/event004.png",
+          "type" : "member",
+          "name" : "SVIP",
+          "desc" : ""
+        },
+        "user_id" : "",
+        "group_name" : "",
+        "deleted" : false,
+        "group_id" : "",
+        "locked" : false,
+        "membership_type" : 0,
+        "role" : 1,
+        "nickname" : ""
+      },
+      "code" : 1
+    }
+}
+
+$done({body : JSON.stringify(obj)});

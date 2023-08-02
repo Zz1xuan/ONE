51fa40f6af63e8941c935ae422bdba3f28bf898f
Zz1xuan <zhouzixuan30@gmail.com>
2023-08-02 12:57:33 +0800

    8.2/12:57

---
+++ Script/AD/ali.js
@@ -0,0 +1,28 @@
+//https://github.com/RuCu6/QuanX/blob/main/Scripts/adrive.js
+
+const url = $request.url;
+if (!$response.body) $done({});
+let obj = JSON.parse($response.body);
+
+if (url.includes("/v2/users/home/widgets")) {
+  const item = [
+    "banners", // 顶部奖励
+    "coreFeatures", // 横版图标
+    "mainBackup", // 手机备份
+    "minorBackup", // 备份设备列表
+    "signIn" // 签到
+  ];
+  item.forEach((i) => {
+    delete obj[i];
+  });
+}
+
+$done({ body: JSON.stringify(obj) });
+
+/* if (url.includes("/v2/users/home/news")) {
+    if (obj.result.length) {
+      obj.result = obj.result.filter(
+        (i) => !i?.code?.includes("productUpdate")
+      );
+    }
+  } else  */

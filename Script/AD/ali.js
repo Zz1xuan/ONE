ee84dda8665a1411d8ec0396ec0f0b7a3a23ddea
Zz1xuan <zhouzixuan30@gmail.com>
2023-08-02 13:03:07 +0800

    .

--- Script/AD/ali.js
+++ Script/AD/ali.js
@@ -4,13 +4,19 @@ const url = $request.url;
 if (!$response.body) $done({});
 let obj = JSON.parse($response.body);
 
-if (url.includes("/v2/users/home/widgets")) {
+if (url.includes("/v2/users/home/news")) {
+  if (obj.result.length) {
+    obj.result = obj.result.filter(
+      (i) => !i?.code?.includes("productUpdate")
+    );
+  }
+} else if (url.includes("/v2/users/home/widgets")) {
   const item = [
-    "banners", // 顶部奖励
+    //"banners", // 顶部奖励
     "coreFeatures", // 横版图标
     "mainBackup", // 手机备份
     "minorBackup", // 备份设备列表
-    "signIn" // 签到
+    //"signIn" // 签到
   ];
   item.forEach((i) => {
     delete obj[i];
@@ -18,11 +24,3 @@ if (url.includes("/v2/users/home/widgets")) {
 }
 
 $done({ body: JSON.stringify(obj) });
-
-/* if (url.includes("/v2/users/home/news")) {
-    if (obj.result.length) {
-      obj.result = obj.result.filter(
-        (i) => !i?.code?.includes("productUpdate")
-      );
-    }
-  } else  */

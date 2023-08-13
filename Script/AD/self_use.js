12d762298f1dc96fb33797e0b7aaffe4f8bb876b
Zz1xuan <zhouzixuan30@gmail.com>
2023-08-13 21:57:17 +0800

    use js

--- Script/AD/self_use.js
+++ Script/AD/self_use.js
@@ -4,7 +4,7 @@ const header = $request.headers;
 let ua = header["User-Agent"] || header["user-agent"];
 
 if (url.includes("/amdc/mobileDispatch")) {
-  if (ua.includes("AMapiPhone") || ua.includes("Cainiao") || ua.includes("%E9%A3%9E%E7%8C%AA%E6%97%85%E8%A1%8C") || ua.includes("Alibaba")) {
+  if (ua.includes("AMapiPhone") || ua.includes("Cainiao") || ua.includes("%E9%A3%9E%E7%8C%AA%E6%97%85%E8%A1%8C") || ua.includes("Alibaba" ) || ua.includes("DMPortal" )) {
     $done({ status: "HTTP/1.1 404 Not Found" });
     return;
   }

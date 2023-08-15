0594add7b3b2c648c6f923c085ee243d1cb1783e
Zz1xuan <zhouzixuan30@gmail.com>
2023-08-15 15:45:43 +0800

    ss

---
+++ Script/AD/self_use_qx.js
@@ -0,0 +1,6 @@
+var ua = $request.headers["User-Agent"] || $request.headers["user-agent"];
+if (-1 !== ua.indexOf("AMap") || -1 !== ua.indexOf("Cainiao") || -1 !== ua.indexOf("%E9%A3%9E%E7%8C%AA%E6%97%85%E8%A1%8C")|| -1 !== ua.indexOf("Alibaba") || -1 !== ua.indexOf("DMPortal") ) {
+  $done({ status: "HTTP/1.1 404 Not Found" });
+} else {
+  $done({});
+}

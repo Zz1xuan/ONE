985e5b811dfadab7cf9139b5022686074b3b23c2
Zz1xuan <zhouzixuan30@gmail.com>
2023-08-24 23:37:22 +0800

    fix

--- Script/AD/cainiao.js
+++ Script/AD/cainiao.js
@@ -1,20 +1,36 @@
-const version = 'V1.0.16';
+//在墨鱼和rucu6两位大佬的文件下进行融合改进
+var obj = JSON.parse($response.body);
+var url = $request.url;
 
+if (/mtop\.cainiao\.nbpresentation\.protocol\.homepage\.get\.cn/.test(url)) {
+    if (obj.data?.result?.dataList?.length > 0) {
+        obj.data.result.dataList = obj.data.result.dataList.filter(d => 
+            d.type !== "big_banner_area_v870" && d.type !== "new_big_banner_area"
+        );
+    }
+} else if (/mtop\.cainiao\.guoguo\.nbnetflow\.ads\.mshow/.test(url)) {
+    if (obj.data["1308"]) delete obj.data["1308"];
+    if (obj.data["1275"]) delete obj.data["1275"];
+} else if (/mtop\.cainiao\.guoguo\.nbnetflow\.ads\.index\.cn/.test(url)) {
+    if (obj.data?.result) obj.data.result = [{}];
+} else if (/mtop\.cainiao\.adkeyword/.test(url)) {
+    if (obj.data?.result?.adHotKeywords) obj.data.result.adHotKeywords = [];
+} else if (/mtop\.cainiao\.guoguo\.nbnetflow\.ads\.show/.test(url)) {
+    if (obj.data?.result) {
+        obj.data.result = obj.data.result.filter(
+            i =>
+            !(
+                i?.materialContentMapper?.adItemDetail ||
+                (i?.materialContentMapper?.bgImg &&
+                    i?.materialContentMapper?.advRecGmtModifiedTime) ||
+                ["common_header_banner", "entertainment", "kuaishou_banner"].includes(
+                    i?.materialContentMapper?.group_id
+                ) ||
+                ["29338", "29339", "32103", "33927"].includes(i.id)
+            )
+        );
+    }
+}
 
-var ddgksf2013=JSON.parse($response.body);
--1!=$request.url.indexOf("mtop.cainiao.nbpresentation.protocol.homepage.get.cn")?
-    ddgksf2013.data?.result?.dataList?.length>0&&
-    (ddgksf2013.data.result.dataList=ddgksf2013.data.result.dataList.filter(d=>"big_banner_area_v870"!=d.type))
-    :
--1!=$request.url.indexOf("mtop.cainiao.guoguo.nbnetflow.ads.mshow")?
-    (ddgksf2013.data["1308"]&&delete ddgksf2013.data["1308"],
-    ddgksf2013.data["1275"]&&delete ddgksf2013.data["1275"])
-    :
--1!=$request.url.indexOf("mtop.cainiao.guoguo.nbnetflow.ads.index.cn")?
-    ddgksf2013.data?.result&&(ddgksf2013.data.result=[{}])
-    :
--1!=$request.url.indexOf("mtop.cainiao.adkeyword")&&
-    ddgksf2013.data?.result?.adHotKeywords&&
-    (ddgksf2013.data.result.adHotKeywords=[]);
-var body=JSON.stringify(ddgksf2013);
-$done({body});
+var body = JSON.stringify(obj);
+$done({ body });

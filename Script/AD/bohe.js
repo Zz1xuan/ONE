f1ea9ce3e2d46b9bad5f76b98fc2159fc5d8a257
Zz1xuan <zhouzixuan30@gmail.com>
2023-08-25 17:02:01 +0800

    ss

--- Script/AD/bohe.js
+++ Script/AD/bohe.js
@@ -1,4 +1,4 @@
-let body = $response.body;
+/* let body = $response.body;
 
 if (body) {
   try {
@@ -13,6 +13,21 @@ if (body) {
   } catch (error) {
     console.error("Error parsing JSON:", error);
   }
-}
+} */
 
 $done({ 'body': body });
+
+var obj = JSON.parse($response.body);
+var url = $request.url;
+
+if (/meta-interface\/v1\/index\/record_index/.test(url)) {
+  if (obj.data) {
+    delete obj.data.articles;
+  }
+} else if (/api\/v1\/shop_homepage/.test(url) && obj.data && obj.data.length > 0) {
+  const idsToRemove = [105, 110, 111, 13, 126, 62, 151, 128, 163, 132, 10, 164];
+  obj.data = obj.data.filter(item => !idsToRemove.includes(item.id));
+}
+
+var body = JSON.stringify(obj);
+$done({ body });

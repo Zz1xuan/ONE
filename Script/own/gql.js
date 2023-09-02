001a30f7aecc9ba99ef74a506fb52b58616d398c
Zz1xuan <zhouzixuan30@gmail.com>
2023-09-02 12:51:39 +0800

    add

--- Script/own/gql.js
+++ Script/own/gql.js
@@ -1,6 +1,6 @@
 const modifiedBody = $response.body
   .replace(/"user_is_pro":\w+/g, '"user_is_pro":true')
   .replace(/"group_is_pro":\w+/g, '"group_is_pro":true')
-  .replace(/"membership_type":\w+/g, '"membership_type":2');
+  //.replace(/"membership_type":\w+/g, '"membership_type":2');
 
 $done({ body: modifiedBody });

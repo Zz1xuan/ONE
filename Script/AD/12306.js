7d25f97121bd213b7f9c107c502abc3b3b702136
Zz1xuan <zhouzixuan30@gmail.com>
2023-09-06 12:48:56 +0800

    bing

--- Script/AD/12306.js
+++ Script/AD/12306.js
@@ -17,7 +17,7 @@ hostname = ad.12306.cn
 !function() {
   let e, i = JSON.parse($request.body);
   e = "0007" === i.placementNo 
-    ? '{"materialsList":[{"billMaterialsId":"255","filePath":"h","creativeType":1}],"advertParam":{"skipTime":1}}' 
+    ? '{"materialsList":[{"billMaterialsId":"255","filePath":"https://www.bing.com/HPImageArchive.aspx?format=js&idx=0&n=1&mkt=en-CN","creativeType":1}],"advertParam":{"skipTime":1}}' 
     : "G0054" === i.placementNo 
       ? '{"code":"00","materialsList":[{}],"advertParam":{"skipTime":3000,"showSkipBtn":0,"skipTimeAgain":0,"chacheTime":600000,"fixedscreen":3,"isDefault":0,"displayNumDi":1,"index":2}}' 
       : '{"code":"00","message":"无广告返回"}';

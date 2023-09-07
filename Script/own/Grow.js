341d4feb6fe1315765b4fac5c4adb0ad5486ccf6
Zz1xuan <zhouzixuan30@gmail.com>
2023-09-07 10:19:55 +0800

    a

--- Script/own/Grow.js
+++ Script/own/Grow.js
@@ -5,7 +5,7 @@ const modifiedBody = bodyText.replace(/"entitlements": {}/g, `{
     "grow.pro": {
         "expires_date": "2023-09-14T01:20:15Z",
         "grace_period_expires_date": null,
-        "product_identifier": "grow_lifetime",
+        "product_identifier": "grow_1y_128",
         "purchase_date": "2023-09-07T01:20:15Z"
     }
 }`);
@@ -15,15 +15,15 @@ const modifiedBody2 = modifiedBody.replace(/"management_url": null/g, `"manageme
 
 // 替换 "subscriptions": {} 为指定的 JSON 对象
 const modifiedBody3 = modifiedBody2.replace(/"subscriptions": {}/g, `{
-    "grow_lifetime": {
+    "grow_1y_128": {
         "auto_resume_date": null,
         "billing_issues_detected_at": null,
-        "expires_date": null,
+        "expires_date": "2024-09-07T01:20:15Z",
         "grace_period_expires_date": null,
         "is_sandbox": false,
         "original_purchase_date": "2023-09-07T01:20:15Z",
         "ownership_type": "PURCHASED",
-        "period_type": "lifetime",
+        "period_type": "annual",
         "purchase_date": "2023-09-07T01:20:15Z",
         "refunded_at": null,
         "store": "app_store",

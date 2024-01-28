94121efb5337d7b66bb5484d2334f22561b1b1ec
Zz1xuan <zhouzixuan30@gmail.com>
2024-01-28 12:20:40 +0800

    update

--- Script/AD/ali.js
+++ Script/AD/ali.js
@@ -14,7 +14,7 @@ if (url.includes("/v2/users/home/news")) {
     const item = [
         "banners", // 顶部奖励
         //"coreFeatures", // 横版图标
-        //"mainBackup", // 手机备份
+        "mainBackup", // 手机备份
         //"minorBackup", // 备份设备列表
         //"signIn" // 签到
     ];

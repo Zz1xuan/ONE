4eb4a5c93062f052ed3f8d58837e9777bd74d0d3
Zz1xuan <zhouzixuan30@gmail.com>
2024-07-20 09:22:59 +0800

    a

--- Script/AD/1.js
+++
@@ -1,9 +0,0 @@
-let jsonData = $response.body;
-try {
-    let parsedData = JSON.parse(jsonData);
-    delete context;
-    jsonData = JSON.stringify(parsedData);
-} catch (error) {
-    console.log("Error:", error);
-}
-$done({body: jsonData});

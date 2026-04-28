f04120106b75c2c03aa4be22e5d6fee83b366a28
Zz1xuan <103792636+Zz1xuan@users.noreply.github.com>
2026-04-28 19:45:57 +0800

    Rollback Reddit response script to separate version

--- Script/AD/reddit.js
+++ Script/AD/reddit.js
@@ -1,70 +1,61 @@
 // 一佬
-// Reddit 去广告 + 关 NSFW + 自动翻译
+// Reddit 去广告 + 关 NSFW
 
-if (typeof $request !== 'undefined' && $request && $request.headers) {
-  var headers = $request.headers;
-  delete headers['x-reddit-translations'];
-  delete headers['X-Reddit-Translations'];
-  headers['x-reddit-translations'] = 'enabled, seo, zh-hans';
-  $done({ headers: headers });
-} else if (typeof $response !== 'undefined' && $response && $response.body) {
-  function walk(value) {
-    if (Array.isArray(value)) {
-      var out = [];
-      for (var i = 0; i < value.length; i++) {
-        var next = walk(value[i]);
-        if (next !== undefined) out.push(next);
-      }
-      return out;
+function walk(value) {
+  if (Array.isArray(value)) {
+    var out = [];
+    for (var i = 0; i < value.length; i++) {
+      var next = walk(value[i]);
+      if (next !== undefined) out.push(next);
     }
+    return out;
+  }
 
-    if (value && typeof value === 'object') {
-      for (var key in value) {
-        if (Object.prototype.hasOwnProperty.call(value, key)) {
-          var child = walk(value[key]);
-          if (child === undefined) {
-            delete value[key];
-          } else {
-            value[key] = child;
-          }
+  if (value && typeof value === 'object') {
+    for (var key in value) {
+      if (Object.prototype.hasOwnProperty.call(value, key)) {
+        var child = walk(value[key]);
+        if (child === undefined) {
+          delete value[key];
+        } else {
+          value[key] = child;
         }
       }
-
-      if (value.isNsfw === true) value.isNsfw = false;
-      if (value.isNsfwMediaBlocked === true) value.isNsfwMediaBlocked = false;
-      if (value.isNsfwContentShown === false) value.isNsfwContentShown = true;
-      if (Array.isArray(value.commentsPageAds)) value.commentsPageAds = [];
-
-      if (
-        value.node &&
-        typeof value.node === 'object' &&
-        Array.isArray(value.node.cells) &&
-        value.node.cells.some(function (cell) {
-          return cell && (cell.__typename === 'AdMetadataCell' || cell.isAdPost === true);
-        })
-      ) {
-        return undefined;
-      }
-
-      if (value.node && typeof value.node === 'object' && value.node.adPayload) {
-        return undefined;
-      }
-
-      if (value.__typename === 'AdPost') {
-        return undefined;
-      }
     }
 
-    return value;
+    if (value.isNsfw === true) value.isNsfw = false;
+    if (value.isNsfwMediaBlocked === true) value.isNsfwMediaBlocked = false;
+    if (value.isNsfwContentShown === false) value.isNsfwContentShown = true;
+    if (Array.isArray(value.commentsPageAds)) value.commentsPageAds = [];
+
+    if (
+      value.node &&
+      typeof value.node === 'object' &&
+      Array.isArray(value.node.cells) &&
+      value.node.cells.some(function (cell) {
+        return cell && (cell.__typename === 'AdMetadataCell' || cell.isAdPost === true);
+      })
+    ) {
+      return undefined;
+    }
+
+    if (value.node && typeof value.node === 'object' && value.node.adPayload && typeof value.node.adPayload === 'object') {
+      return undefined;
+    }
+
+    if (value.__typename === 'AdPost') {
+      return undefined;
+    }
   }
 
-  try {
-    var body = JSON.parse($response.body);
-    $done({ body: JSON.stringify(walk(body)) });
-  } catch (e) {
-    console.log('reddit.js parse error: ' + e);
-    $done({});
-  }
-} else {
+  return value;
+}
+
+try {
+  var body = JSON.parse($response.body);
+  var result = walk(body);
+  $done({ body: JSON.stringify(result) });
+} catch (e) {
+  console.log('reddit.js parse error: ' + e);
   $done({});
 }

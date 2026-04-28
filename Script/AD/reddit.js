125e3084c212c6675ed5904334c54ce54ab6936a
Zz1xuan <103792636+Zz1xuan@users.noreply.github.com>
2026-04-28 15:18:56 +0800

    Restore Reddit response-body script

--- Script/AD/reddit.js
+++ Script/AD/reddit.js
@@ -1,7 +1,30 @@
-// Reddit 自动翻译
+// Reddit 过滤推广, 关 subreddit 的 NSFW 提示
 
-var headers = $request.headers || {};
-delete headers["x-reddit-translations"];
-delete headers["X-Reddit-Translations"];
-headers["x-reddit-translations"] = "enabled, seo, zh-hans";
-$done({ headers });
+let modified;
+let body;
+try {
+  body = JSON.parse($response.body.replace(/\"isNsfw\"/gi, '"_isNsfw"'));
+  if (body?.data?.subredditInfoByName?.elements?.edges) {
+    body.data.subredditInfoByName.elements.edges =
+      body.data.subredditInfoByName.elements.edges.filter(
+        i => i?.node?.__typename !== 'AdPost'
+      );
+    modified = true;
+  } else if (body?.data?.home?.elements?.edges) {
+    body.data.home.elements.edges = body.data.home.elements.edges.filter(
+      i => i?.node?.__typename !== 'AdPost'
+    );
+    modified = true;
+  } else if (body?.data?.homeV3?.elements?.edges) {
+    body.data.homeV3.elements.edges = body.data.homeV3.elements.edges.filter(
+      i => !i?.node?.cells?.some(j => j?.__typename === 'AdMetadataCell')
+    );
+    modified = true;
+  } else if ($response.body.includes('"isNsfw"')) {
+    modified = true;
+  }
+} catch (e) {
+  console.log(e);
+} finally {
+  $done(modified ? { body: JSON.stringify(body) } : {});
+}

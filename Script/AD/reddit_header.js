c9164bc0bd27b599f7e474368d26c7e01c243f1b
Zz1xuan <103792636+Zz1xuan@users.noreply.github.com>
2026-04-28 16:51:35 +0800

    Remove redundant Reddit header script

--- Script/AD/reddit_header.js
+++
@@ -1,7 +0,0 @@
-// Reddit 自动翻译
-
-var headers = $request.headers || {};
-delete headers["x-reddit-translations"];
-delete headers["X-Reddit-Translations"];
-headers["x-reddit-translations"] = "enabled, seo, zh-hans";
-$done({ headers });

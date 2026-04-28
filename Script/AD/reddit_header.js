b5cb4171d17b5acfc5aa9b54f4d71afd71611705
Zz1xuan <103792636+Zz1xuan@users.noreply.github.com>
2026-04-28 18:14:59 +0800

    Restore Reddit header translation script

---
+++ Script/AD/reddit_header.js
@@ -0,0 +1,8 @@
+// 一佬
+// Reddit 自动翻译
+
+var headers = $request.headers || {};
+delete headers["x-reddit-translations"];
+delete headers["X-Reddit-Translations"];
+headers["x-reddit-translations"] = "enabled, seo, zh-hans";
+$done({ headers });
